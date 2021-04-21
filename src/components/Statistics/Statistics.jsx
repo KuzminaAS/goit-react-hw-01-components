8fe6f891d12dcb61de6250f141dbe2e75f4846a5
Sasha <protzenko.as@gmail.com>
2021-04-21 09:21:50 +0300

    add random function

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 
 import styles from './Statistics.module.css';
 
+import randColor from '../randomColor.js'
 
 const Statistics = ({title, stats}) => {
     return (
@@ -11,7 +12,7 @@ const Statistics = ({title, stats}) => {
   <h2 className={styles.title}>{title}</h2>
 
  <ul className={styles.list}> {stats.map((stat) => (
- <li className={styles.item} key={stat.id}>
+ <li className={styles.item} key={stat.id} style={{backgroundColor: randColor()}}>
       <span className={styles.label}>{stat.label}</span>
       <span className={styles.percentage}>{stat.percentage}</span>
     </li>
